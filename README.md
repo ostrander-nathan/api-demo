6f5b400d0496f45a77ea64926734ebd6173dc719
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-04-27 11:56:40 -0500

    clean & readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # api-demo
+
+### Description: 
+
+* Introduction into Api calls
+
+### Final Result:
+> Single Page showing api call 
+
+### Take-home Lesson:
+
+> Basic Api call with notes in code
+
+
+### How to run (Node must be installed on your machine):
+```
+git clone https://github.com/ostrander-nathan/api-demo
+npm install http-server -g
+http-server -p 8080
+```
+
+
+### Contributors:
+[Nathan Ostrander](https://github.com/ostrander-nathan)
--- javascripts/main.js
+++ javascripts/main.js
@@ -3,50 +3,50 @@
 let apiKeys = {};
 
 let imageList = (searchText) => { // takes input with promise
-	return new Promise ((resolve,reject) => {
-		$.ajax({
-			method: "GET",
-			url: "apiKeys.json"
-		}).then((response) =>{ // get keys
-			// console.log("response",response);
-			apiKeys = response;
-			let authHeader = "Client-ID " + apiKeys.client_id;
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            method: "GET",
+            url: "apiKeys.json"
+        }).then((response) => { // get keys
+            // console.log("response",response);
+            apiKeys = response;
+            let authHeader = "Client-ID " + apiKeys.client_id;
 
 
-$.ajax({ // get 2 call 
-	method:"GET",
-	headers:{
-		"Authorization": authHeader
-	},
-	url: `https://api.imgur.com/3/gallery/t/${searchText}`,
-}).then((response2)=>{
-	// console.log("imgur response",response2.data.items );
-	resolve(response2.data.items);
-},(errorResponse2)=>{
-	// console.log("imgur fail",errorResponse2 );
-	reject(errorResponse2);
-});
+            $.ajax({ // get 2 call 
+                method: "GET",
+                headers: {
+                    "Authorization": authHeader
+                },
+                url: `https://api.imgur.com/3/gallery/t/${searchText}`,
+            }).then((response2) => {
+                // console.log("imgur response",response2.data.items );
+                resolve(response2.data.items);
+            }, (errorResponse2) => {
+                // console.log("imgur fail",errorResponse2 );
+                reject(errorResponse2);
+            });
 
 
-		  }, (errorResponse)=>{
-		   // console.log("errorResponse",errorResponse);
-		  });
-		});
-	};
+        }, (errorResponse) => {
+            // console.log("errorResponse",errorResponse);
+        });
+    });
+};
 
-$(document).ready(function(){
-	$("#clicky-button").on("click",()=>{
-		$("#clicky-button").button("loading");
-		$("#output").html("");
-	let searchy = $("#imgur-search").val();
-console.log("its working", searchy);
-imageList(searchy).then((dataFromImgur)=>{
-	$("#clicky-button").button("reset");
-	console.log("dataFromImgur",dataFromImgur);
-	dataFromImgur.forEach((image)=>{
-		$("#output").append(`<img src="${image.link}">`);
-	});
-	});
-	});
+$(document).ready(function() { // dom maniupulation 
+    $("#clicky-button").on("click", () => {
+        $("#clicky-button").button("loading");
+        $("#output").html("");
+        let searchy = $("#imgur-search").val();
+        console.log("its working", searchy);
+        imageList(searchy).then((dataFromImgur) => {
+            $("#clicky-button").button("reset");
+            console.log("dataFromImgur", dataFromImgur);
+            dataFromImgur.forEach((image) => {
+                $("#output").append(`<img src="${image.link}">`);
+            });
+        });
+    });
 
 });
